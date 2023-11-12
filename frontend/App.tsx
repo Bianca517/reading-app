9c714c59340655a76810194df9fdd681a3bef02b
Bianca Teregan <biancateregan@gmail.com>
2023-11-12 14:59:11 +0200

    before deleting node modules

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,7 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginPageUI from "./UI/login/login-ui"
 import HomePageUI from "./UI/home/home-ui"
-import TestPage from './UI/test/face-detector';
+import FaceDetector from './UI/test/face-detector';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,10 +10,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-         screenOptions={{
+        screenOptions={{
           headerShown: false
         }}>
-        <Stack.Screen name="Login" component={TestPage} />
+        <Stack.Screen name="Login" component={FaceDetector} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- frontend/UI/test/face-detector.js
+++ frontend/UI/test/face-detector.js
@@ -3,74 +3,89 @@ import * as React from 'react';
 import * as tf from '@tensorflow/tfjs';
 import * as coco_model from '@tensorflow-models/coco-ssd';
 
-import { useState, useEffect } from 'react';
-import { View, Text, Button, StyleSheet, PermissionsAndroid } from 'react-native';
-import { Camera } from 'expo-camera'; // You'll need to install the Camera component if you're using Expo
+import { useState } from 'react';
+import { View, Text, Button, StyleSheet, Alert } from 'react-native';
+import { Camera } from 'expo-camera';
+import { RNCamera } from 'react-native-camera';
 
-const App = () => {
-    const [webcamEnabled, setWebcamEnabled] = useState(false);
-    const [modelLoaded, setModelLoaded] = useState(true); // Simulating that the model is already loaded
-  
-    // Check if webcam access is supported.
-    const getUserMediaSupported = async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      return status === 'granted';
-    };
-  
-    // Function to enable the webcam.
-    const enableCam = async () => {
-      if (await getUserMediaSupported()) {
-        setWebcamEnabled(true);
-      } else {
-        console.warn('Camera access is not granted.');
-      }
-    };
-  
-    // Placeholder function for object detection using the webcam (use TensorFlow.js or other library here).
-    const predictWebcam = async () => {
-      // Implement object detection using the webcam here.
-    };
-  
-    // This useEffect simulates that the model is loaded.
-    useEffect(() => {
-      setModelLoaded(true);
-    }, []);
-  
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Multiple object detection using TensorFlow.js</Text>
-        <Text>Wait for the model to load before clicking the button to enable the webcam.</Text>
-  
-       
-          <View style={styles.webcamView}>
-            {webcamEnabled ? (
-              <Camera style={styles.webcam} type={Camera.Constants.Type.front} onCameraReady={predictWebcam} />
-            ) : (
-              <Button title="Enable Webcam" onPress={enableCam} />
-            )}
-          </View>
-        
-      </View>
-    );
+const FaceDetector = () => {
+  const [webcamEnabled, setWebcamEnabled] = useState(false);
+  const [modelLoaded, setModelLoaded] = useState(true); // Simulating that the model is already loaded
+
+  React.useEffect(() => {
+    getUserCameraPermission();
+    console.warn("Current state of webcam: " + webcamEnabled + "\n");
+  })
+
+  // Check if webcam access is supported.
+  const getUserMediaSupported = async () => {
+    const { status } = await Camera.requestPermissionsAsync();
+    return status === 'granted';
   };
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    title: {
-      fontSize: 24,
-      marginBottom: 20,
-    },
-    webcamView: {
-      alignItems: 'center',
-    },
-    webcam: {
-      width: 320,
-      height: 240,
-    },
-  });
-  
-  export default App;
+
+  function getUserCameraPermission() {
+    return Alert.alert(title = 'Allow camera access?',
+      [
+        {
+          text: 'No',
+          onPress: () => { setWebcamEnabled(false) },
+        },
+        {
+          text: 'Yes',
+          onPress: () => { setWebcamEnabled(true) },
+        },
+      ])
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Testing TensorFlow Face Detection</Text>
+
+      <View style={styles.webcamView}>
+        {webcamEnabled ? (
+
+          <Camera
+            style={styles.webcam}
+            type={Camera.Constants.Type.front}
+            onCameraReady={predictWebcam}
+          />
+
+          /*
+           <RNCamera
+             ref={ref => {
+               this.camera = ref;
+             }}
+             style={styles.webcam} 
+             type={RNCamera.Constants.Type.front}
+             onCameraReady={predictWebcam}
+           />
+           */
+        ) : (
+          <Text>Webcam not enabled</Text>
+        )}
+      </View>
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  webcamView: {
+    alignItems: 'center',
+  },
+  webcam: {
+    width: 320,
+    height: 240,
+  },
+});
+
+export default FaceDetector;
