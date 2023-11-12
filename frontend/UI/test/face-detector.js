caf0cc1198f58b59b06e87b5e8887a148c15ed6f
Bianca Teregan <biancateregan@gmail.com>
2023-11-12 19:56:24 +0200

    more tries to do the automatic scroll. not working because of delays in timing

--- frontend/UI/test/face-detector.js
+++ frontend/UI/test/face-detector.js
@@ -1,44 +1,76 @@
 
 import * as React from 'react';
-import * as tf from '@tensorflow/tfjs';
-import * as coco_model from '@tensorflow-models/coco-ssd';
 
-import { useState } from 'react';
+import { useState, useEffect, setState } from 'react';
 import { View, Text, Button, StyleSheet, Alert } from 'react-native';
-import { Camera } from 'expo-camera';
+import { Camera, CameraOrientation, CameraType } from 'expo-camera';
 import { RNCamera } from 'react-native-camera';
+import * as FileSystem from 'expo-file-system';
+import BackgroundTimer from 'react-native-background-timer';
 
 const FaceDetector = () => {
   const [webcamEnabled, setWebcamEnabled] = useState(false);
-  const [modelLoaded, setModelLoaded] = useState(true); // Simulating that the model is already loaded
+  const cameraReference = React.useRef(null);
 
-  React.useEffect(() => {
+  [currentTime, setCurrentTime] = useState(new Date());
+  [cameraReady, setCameraReady] = useState(false);
+
+  //did this so the first thing when user opens this page, is the alert for asking for permission
+  useEffect(() => {
     getUserCameraPermission();
-    console.warn("Current state of webcam: " + webcamEnabled + "\n");
-  })
+  }, []);
 
-  // Check if webcam access is supported.
-  const getUserMediaSupported = async () => {
-    const { status } = await Camera.requestPermissionsAsync();
-    return status === 'granted';
+  setInterval(async () => {
+    this.setCurrentTime(new Date());
+    console.log('Current time ' + currentTime.getMinutes() + "m : " + currentTime.getSeconds() + "s" + " : " + currentTime.getMilliseconds() + "ms");
+    //console.log("in time st interval\n");
+  }, 500);
+
+  const removePreviousPicture = async (previousPictureURI) => {
+    try {
+      await FileSystem.deleteAsync(previousPictureURI);
+      console.log('File deleted');
+    } catch (error) {
+      console.error('Failed to delete file:', error);
+    }
+  }
+
+  const takePictureWithCamera = () => {
+    BackgroundTimer.runBackgroundTimer(() => {
+      if (webcamEnabled && cameraReference.current) {
+        cameraReference.current.takePictureAsync({
+          quality: 0.1,
+          skipProcessing: true,
+          base64: true
+        })
+          .then(photo => {
+            // Process the photo if needed
+            removePreviousPicture(photo.uri); // You can use your remove function here
+            console.log('Took Picture:', new Date());
+          })
+          .catch(error => {
+            console.error('Failed to take picture:', error);
+          });
+      }
+    }, 1000); // Interval time in milliseconds
   };
 
   function getUserCameraPermission() {
-    return Alert.alert(title = 'Allow camera access?',
+    return Alert.alert(title = '"App_name" Would Like to Access the Camera', message = 'This will be used for gesture scrolling',
       [
         {
-          text: 'No',
+          text: "Don't Allow",
           onPress: () => { setWebcamEnabled(false) },
         },
         {
-          text: 'Yes',
+          text: 'OK',
           onPress: () => { setWebcamEnabled(true) },
         },
       ])
   }
 
   return (
-    <View style={styles.container}>
+    <View style={styles.full_container}>
       <Text style={styles.title}>Testing TensorFlow Face Detection</Text>
 
       <View style={styles.webcamView}>
@@ -46,31 +78,37 @@ const FaceDetector = () => {
 
           <Camera
             style={styles.webcam}
-            type={Camera.Constants.Type.front}
-            onCameraReady={predictWebcam}
+            type={CameraType.front}
+            ref={cameraReference}
+            onCameraReady={takePictureWithCamera}
           />
 
           /*
-           <RNCamera
-             ref={ref => {
-               this.camera = ref;
-             }}
-             style={styles.webcam} 
-             type={RNCamera.Constants.Type.front}
-             onCameraReady={predictWebcam}
-           />
-           */
+         <RNCamera
+           ref={ref => {
+             this.camera = ref;
+           }}
+           style={styles.webcam} 
+           type={RNCamera.Constants.Type.front}
+           onCameraReady={predictWebcam}
+         />
+         */
         ) : (
           <Text>Webcam not enabled</Text>
         )}
       </View>
 
+      <View style={styles.results_container}>
+        <Text>
+          current_time: {currentTime.getMinutes()}m : {currentTime.getSeconds()}s : {currentTime.getMilliseconds()}ms
+        </Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  full_container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
@@ -83,9 +121,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   webcam: {
-    width: 320,
-    height: 240,
+    width: 340,
+    height: 600,
   },
+  results_container: {
+    alignItems: 'center', //horizontally
+    justifyContent: 'center', //vertically
+    height: 100,
+    width: '100%',
+    backgroundColor: '#FFB6C1'
+  }
 });
 
 export default FaceDetector;
